4ac315c6920f002fd2dbfdc4fb02293704f48d50
Moussa FOFANA <didegassama@gmail.com>
2021-11-18 12:34:41 +0000

    implementing schedules for programme closing data reminders

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('remind:programme:closing')->dailyAt('10:00')->timezone('Africa/Dakar');
+        $schedule->command('remind:programme:closing')->dailyAt('20:00')->timezone('Africa/Dakar');
     }
 
     /**
