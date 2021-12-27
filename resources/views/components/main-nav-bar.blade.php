12ed7447004931c816c892dc0f66be3e88014ef0
Moussa FOFANA <didegassama@gmail/com>
2021-12-27 11:45:52 +0000

    https://bambogroup.atlassian.net/browse/SUBS-119

--- resources/views/components/main-nav-bar.blade.php
+++ resources/views/components/main-nav-bar.blade.php
@@ -2,61 +2,17 @@
 
     <div class="left-menu">
         <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
-            <li class="nav-item">
-                <a class="nav-link link text-primary display-4" href="{{ route('apropos') }}">A
-                    propos</a>
-            </li>
-        </ul>
-    </div>
-    <div class="brand-container">
-        <div class="navbar-brand">
-            <span class="navbar-logo">
-                <a href="{{ route('home') }}">
-                    <img src="{{ asset('assets/images/fulllogo-nobuffer-612x123.png') }}" alt=""
-                        style="height: 2.5rem;">
-                </a>
-            </span>
-        </div>
-    </div>
-    <div class="right-menu">
-        <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
-            @auth
-                <li class="nav-item dropdown">
-                    <a class="nav-link link dropdown-toggle text-primary display-4" href="#"
-                        data-toggle="dropdown-submenu" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-                        aria-expanded="false">
-                        Mon compte
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown-undefined">
-                        <a class="dropdown-item text-primary display-4" href="{{ route('achatsms.create') }}"
-                            aria-expanded="false">
-                            Acheter pack SMS
-                        </a>
-                        <form action="{{ route('logout') }}" method="post">
-                            @csrf
-                            <button class="dropdown-item text-primary display-4">
-                                Se déconnecter
-                            </button>
-                        </form>
-                    </div>
-                </li>
-            @endauth
-            <li class="nav-item">
-                <a class="nav-link link text-primary display-4" href="{{ route('contact') }}">
-                    Contacts
-                </a>
-            </li>
             @auth
                 @if (auth()->user()->type == 'admin')
                     <li class="nav-item dropdown">
                         <a class="nav-link link dropdown-toggle text-primary display-4" href="#"
-                            data-toggle="dropdown-submenu" data-bs-toggle="dropdown"
-                            data-bs-auto-close="outside" aria-expanded="false">
+                            data-toggle="dropdown-submenu" data-bs-toggle="dropdown" data-bs-auto-close="outside"
+                            aria-expanded="false">
                             Admin
                         </a>
                         <div class="dropdown-menu" aria-labelledby="dropdown-undefined">
-                            <a class="dropdown-item text-primary display-4"
-                                href="{{ route('admin.programme.index') }}" aria-expanded="false">
+                            <a class="dropdown-item text-primary display-4" href="{{ route('admin.programme.index') }}"
+                                aria-expanded="false">
                                 Programmes
                             </a>
                             <a class="dropdown-item text-primary display-4"
@@ -68,6 +24,49 @@
                         </div>
                     </li>
                 @endif
+                <li class="nav-item">
+                    <a class="nav-link link text-primary display-4" href="{{ route('apropos') }}">A
+                        propos</a>
+                </li>
+            </ul>
+        </div>
+        <div class="brand-container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('assets/images/fulllogo-nobuffer-612x123.png') }}" alt=""
+                            style="height: 2.5rem;">
+                    </a>
+                </span>
+            </div>
+        </div>
+        <div class="right-menu">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                <li class="nav-item">
+                    <a class="nav-link link text-primary display-4" href="{{ route('contact') }}">
+                        Contacts
+                    </a>
+                </li>
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link link dropdown-toggle text-primary display-4" href="#" data-toggle="dropdown-submenu"
+                            data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                            Mon compte
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-undefined">
+                            <a class="dropdown-item text-primary display-4" href="{{ route('achatsms.create') }}"
+                                aria-expanded="false">
+                                Acheter pack SMS
+                            </a>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button class="dropdown-item text-primary display-4">
+                                    Se déconnecter
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                @endauth
             @endauth
         </ul>
     </div>
